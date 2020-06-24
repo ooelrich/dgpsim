108622307f0b25caea54f0bec934f2df44740bce
Oscar Oelrich <oscar.oelrich@live.com>
2020-06-24 14:29:10 +0200

    Changed wording of dgp-function

--- R/dgp.R
+++ R/dgp.R
@@ -1,8 +1,8 @@
 #' Generates data from a linear model
 #'
-#' Generates data on where the outcome is the sum of nCov uniform(-1, 1) random
-#' variables. All covariates are set to 0 by default, then a number corresponding
-#' to nNonZero are changed to one.
+#' Generates nObs observations where the outcome (y) is the sum of nNonZero  out of
+#' nCov uniform(-1, 1) random variables. The covariates with non-zero coefficients
+#' are always the nNonZero first covariates.
 #'
 #' @param nObs Number of observations
 #' @param nCov Number of covariates, excluding intercept
@@ -10,10 +10,8 @@
 #' to right
 
 dgp <- function(nObs, nCov, nNonZero){
-  X <-matrix(runif(nObs * nCov, min = -1, max = 1), nrow = nObs)
+  X <- matrix(runif(nObs * nCov, min = -1, max = 1), nrow = nObs)
   y <- rowSums(X[, 0:nNonZero, drop = FALSE]) + rnorm(nObs, mean = 0, sd = 1)
   df <- data.frame(y, X)
   return(df)
 }
-
-
