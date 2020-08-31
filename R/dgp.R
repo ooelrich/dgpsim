029c72f6b35cc75b50662bb69fa8c56179e4e936
Oscar Oelrich <oscar.oelrich@stat.su.se>
2020-08-31 14:06:21 +0200

    updated dgp function

--- R/dgp.R
+++ R/dgp.R
@@ -8,10 +8,15 @@
 #' @param nCov Number of covariates, excluding intercept
 #' @param nNonZero Number of covariates with coefficients set to one, from left
 #' to right
+#' @param trueMean If TRUE, returns response/outcome without noise. Defaults to
+#' false
 
-dgp <- function(nObs, nCov, nNonZero){
+dgp <- function(nObs, nCov, nNonZero, trueMean = FALSE){
   X <- matrix(runif(nObs * nCov, min = -1, max = 1), nrow = nObs)
-  y <- rowSums(X[, 0:nNonZero, drop = FALSE]) + rnorm(nObs, mean = 0, sd = 1)
+  y <- rowSums(X[, 0:nNonZero, drop = FALSE])
+  if (trueMean == FALSE) {
+    y <- y + rnorm(nObs, mean = 0, sd = 1)
+  }
   df <- data.frame(y, X)
   return(df)
 }
